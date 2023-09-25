7a7dc5c82a361f3b1ff2ee7a781cb633598b8e24
Cyvadra <dj@lifesdk.com>
2023-09-25 14:43:35 +0800

    feat: new DataType ==> an empty DataType object

---
+++ src/suggar.jl
@@ -0,0 +1,18 @@
+
+emptyInitials = Dict{DataType, Any}(
+	String => "",
+	Vector{String} => String[""],
+	Vector{UInt8} => UInt8[],
+	Threads.SpinLock => Threads.SpinLock(),
+	DataType => UInt8,
+	);
+
+function new(data_type::T) where T <: DataType
+	data_type( map(t->haskey(emptyInitials,t) ? deepcopy(emptyInitials[t]) : zero(t), data_type.types)... )
+	end
+
+
+
+
+
+
