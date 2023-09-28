1891612e82dab90cfe08059146d39e9cf22e2e3e
Cyvadra <dj@lifesdk.com>
2023-09-28 18:23:36 +0800

    disable cache by default

--- src/neuron_runtime.jl
+++ src/neuron_runtime.jl
@@ -37,14 +37,14 @@ function RegisterNeuronSimple(f::Function, updated_ts::Int64, output_type::DataT
 	tmpNames = eachmatch(r"[\(, ](.+?)::.+?[,\)]", tmpNames) |> collect
 	tmpNames = map(x->string(x.captures[1]), tmpNames)
 	tmpTypes = Vector{DataType}(collect(tmpMeta.sig.types[2:end]))
-	return RegisterNeuron( string(tmpMeta.name), "auto generated", updated_ts, tmpNames, tmpTypes, output_type, 3, 60, true, true, 10.0, f )
+	return RegisterNeuron( string(tmpMeta.name), "auto generated", updated_ts, tmpNames, tmpTypes, output_type, 3, 60, false, true, 10.0, f )
 	end
 
 function RegisterNeuronAuto(name::String, desc::String, updated_ts::Int64, input_names::Vector, output_type::DataType, calculation::Function)
 	@assert length(methods(calculation)) == 1
 	tmpMeta  = methods(calculation).ms[1]
 	tmpTypes = Vector{DataType}(collect(tmpMeta.sig.types[2:end]))
-	return RegisterNeuron( name, desc, updated_ts, input_names, tmpTypes, output_type, 3, 60, true, true, 10.0, calculation )
+	return RegisterNeuron( name, desc, updated_ts, input_names, tmpTypes, output_type, 3, 60, false, true, 10.0, calculation )
 	end
 
 function SetNeuronParams(name::String, min_update_seconds::Int64, min_update_cache_seconds::Int64, flag_allow_cache::Bool, flag_autotrigger::Bool, weight_priority::Float64)
