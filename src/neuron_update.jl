4dee4868f79beed05ca63b20e35854177d368e43
Cyvadra <dj@lifesdk.com>
2023-09-30 16:52:49 +0800

    init update framework

---
+++ src/neuron_update.jl
@@ -0,0 +1,114 @@
+
+ReviseLock = Threads.SpinLock();
+ReviseList = UInt128[];
+ReviseLockDelayed = Threads.SpinLock();
+ReviseListDelayed = UInt128[];
+
+function ReviseDownstream(ids::Vector{UInt128})::Nothing
+	lock(ReviseLock)
+	append!(ReviseList, ids)
+	unlock(ReviseLock)
+	return nothing
+	end
+
+function Revise(UUID::UInt128)::Bool
+	# from top to bot
+	# must only be triggered automatically
+	n = Network[UUID]
+	# cache check
+		if n.Params[].SwitchAllowCache && round(Int,time()) - n.Cache[].LastUpdatedTimestamp < n.Params[].MinUpdateIntervalSeconds
+			return n.Cache[].LastResult
+		end
+	# calculation
+		lock(n.Cache[].ProcessLock)
+		try
+			n.Cache[].LastResult = n.Cache[].Calculation(
+				map(x->Network[x].Cache[].LastResult[], n.Cache[].UpstreamUUIDs)...
+			) |> Ref
+			n.Cache[].LastUpdatedTimestamp = round(Int,time())
+		catch e
+			@warn e
+			n.Cache[].ErrorLastTs = round(Int,time())
+			n.Cache[].ErrorLastInfo = string(e)
+		finally
+			unlock(n.Cache[].ProcessLock)
+		end
+	# async append new downstream
+		if iszero(n.Cache[].ErrorLastTs)
+			lock(ReviseLockDelayed)
+			append!(ReviseListDelayed, n.Cache[].DownstreamUUIDs)
+			unlock(ReviseLockDelayed)
+		end
+	return iszero(n.Cache[].ErrorLastTs)
+	end
+
+function ExecuteRevision()::Nothing
+	if isempty(ReviseList)
+		return nothing
+	end
+	lock(ReviseLock)
+	# pretreatment
+		sort!(ReviseList, by=x->Network[x].Params[].WeightPriority, rev=true)
+		unique!(ReviseList)
+	# iterate
+		tmpInds = zeros(Bool, length(ReviseList))
+		for i in 1:length(ReviseList)
+			tmpInds[i] = Revise(UUID)
+		end
+	# validate
+		if !all(tmpInds)
+			unlock(ReviseLock)
+			map( UUID->Network[UUID].Cache[].ErrorLastInfo, ReviseList[findall(x->!x,tmpInds)] ) |> join |> throw
+		end
+	# concat
+		lock(ReviseLockDelayed)
+		empty!(ReviseList)
+		append!(ReviseList, ReviseListDelayed)
+		empty!(ReviseListDelayed)
+		unlock(ReviseLockDelayed)
+		unlock(ReviseLock)
+	# next layer
+		if !isempty(ReviseList)
+			return ExecuteRevision()
+		end
+	return nothing
+	end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
