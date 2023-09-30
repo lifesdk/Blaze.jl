46c21801f0d28ce7d81e9b4daa84bb9246745729
Cyvadra <dj@lifesdk.com>
2023-09-30 16:00:21 +0800

    instead of duplicating a new type, reuse Neuron but register those into collector Motivation, so as to update them with time flow

--- src/neuron_runtime.jl
+++ src/neuron_runtime.jl
@@ -6,6 +6,7 @@ struct Neuron
 	end
 
 Network = Dict{UInt128, Neuron}();
+Motivation = Dict{UInt128, Int64}();
 mapNameUUID = Dict{String, UInt128}();
 
 function RegisterNeuron(name::String, desc::String, updated_ts::Int64, input_names::Vector, input_types::Vector{DataType}, output_type::DataType, min_update_seconds::Int64, min_update_cache_seconds::Int64, flag_allow_cache::Bool, flag_autotrigger::Bool, weight_priority::Float64, calculation::Function)::UInt128
@@ -16,10 +17,14 @@ function RegisterNeuron(name::String, desc::String, updated_ts::Int64, input_nam
 	n1 = NeuronBase(name, zero(UInt128), desc, updated_ts, input_names, input_types, output_type)
 	GenerateUUID!(n1)
 	n2 = NeuronParams(min_update_seconds, min_update_cache_seconds, flag_allow_cache, flag_autotrigger, weight_priority)
-	n3 = NeuronCache(calculation, Threads.SpinLock(), UInt128[], UInt128[], round(Int64,time()), Ref(nothing), 0, "")
+	n3 = NeuronCache(calculation, Threads.SpinLock(), UInt128[], UInt128[], 0, Ref(nothing), 0, "")
 	# register network
 	Network[n1.UUID] = Neuron(Ref(n1), Ref(n2), Ref(n3))
 	mapNameUUID[n1.UniqueName] = n1.UUID
+	# register source
+	if isempty(input_names)
+		Motivation[n1.UUID] = n3.LastUpdatedTimestamp + n2.MinUpdateIntervalSeconds
+	end
 	# register stream
 	for c in input_names
 		if !( n1.UUID in Network[ mapNameUUID[c] ].Cache[].DownstreamUUIDs )
