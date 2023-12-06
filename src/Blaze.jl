0d80d84a01857c4b46ef20410223ecf73eb0746b
Cyvadra <dj@lifesdk.com>
2023-12-06 21:32:00 +0800

    add export

--- src/Blaze.jl
+++ src/Blaze.jl
@@ -1,6 +1,12 @@
 module Blaze
 
 export MsgPack
+
+export RegisterNeuron, UpdateNeuron, SetNeuronParams
+export Trigger, AutoTrigger, Revise, ExecuteRevision, AutoExecute
+export ModifyThredSizeAutoExecuteRevision
+export View, LastUpdated, Detail
+
 include("./suggar.jl")
 include("./params.jl")
 include("./neuron.jl")
